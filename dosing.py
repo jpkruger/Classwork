0f217da93cd30067840500b1d5b66d17c4118c9e
Jackson Kruger <jacksonkruger20@gmail.com>
2023-01-27 12:54:30 -0500

    Altered code to make functions more modular

--- dosing.py
+++ dosing.py
@@ -6,7 +6,13 @@
 """
 
 
-def dose_amount():
+def dosing_driver():
+    diagnosis = diagnosis_input()
+    user_weight = weight_input()
+    weight = weight_parse(user_weight)
+    dosage_calculation(diagnosis, weight)
+
+def diagnosis_input():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -15,15 +21,26 @@ def dose_amount():
     print("3 - Community-acquired pneumonia")
     print("4 - Pharyngitis/tonsilitis")
     diagnosis = int(input("Enter a number: "))
+    return diagnosis
+
+def weight_input():
     print("PATIENT WEIGHT")
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
     weight_input = input("Enter weight: ")
+    return weight_input
+
+
+def weight_parse(weight_input):
     weight_data = weight_input.split(" ")
     weight = float(weight_data[0])
     units = weight_data[1]
     if units == "lb":
         weight = weight / 2.205
+    return weight
+
+
+def dosage_calculation(diagnosis,weight):
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
@@ -34,4 +51,4 @@ def dose_amount():
 
 
 if __name__ == '__main__':
-    dose_amount()
+    dosing_driver()
