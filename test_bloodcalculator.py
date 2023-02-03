049b0080a4010fd0656b48717e5b6b7fc68cddbb
Jackson Kruger <jacksonkruger20@gmail.com>
2023-02-03 13:05:04 -0500

    Updated requirements.txt to include pytest and wrote unit tests for LDL and HDL check

---
+++ test_bloodcalculator.py
@@ -0,0 +1,26 @@
+import pytest
+
+@pytest.mark.parametrize("HDL_input, expected",
+[(65, "Normal"),
+ (45, "Borderline Low"),
+ (20, "Low")
+ ])
+def test_HDL_analysis(HDL_input, expected):
+    from bloodcalculator import HDL_analysis
+    # Arrange
+    # Act
+    answer = HDL_analysis(HDL_input)
+    # Assert
+    assert answer == expected
+    
+    
+@pytest.mark.parametrize("LDL_input, expected",
+[(100, "Normal"),
+ (145, "Borderline High"),
+ (175, "High"),
+ (200, "Very High")
+ ])    
+def test_LDL_analysis(LDL_input, expected):
+    from bloodcalculator import LDL_analysis
+    answer = LDL_analysis(LDL_input)
+    assert answer == expected
