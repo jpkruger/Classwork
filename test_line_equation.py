e68632c3863ae80c111a2b99c214e6b66ea64921
Jackson Kruger <jacksonkruger20@gmail.com>
2023-02-04 11:03:07 -0500

    Created unit test for function that determines if point is on a line

--- test_line_equation.py
+++ test_line_equation.py
@@ -1,6 +1,13 @@
+import pytest
 
 
 def test_find_slope(point_inputs, expected):
     from line_equation import find_slope
     answer = find_slope(points_inputs)
-    assert answer = expected 
+    assert answer == expected
+
+
+def test_line_contains_point(point_in, point_desired, expected):
+    from line_equation import test_line_contains_point
+    answer = test_line_contains_points(point_in, point_desired)
+    assert answer == expected    
